3b9aba239295e335525ed1da6c03313cad39d735
Inspirus <44313343+InspirusM@users.noreply.github.com>
2018-11-23 22:48:13 -0800

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,9 +4,9 @@ module.exports.run = async (client, msg, args) => {
 
   let inviteembed = new Discord.RichEmbed()
       .setTitle('You Want Me In Your Server?')
-      .setDescription('Hello Buddies! You Want A Amazing Bot For Your Server ! Click Below To Invite Me!')
+      .setDescription('Hello Buddies! You Want A Amazing Bot For Your Server.Click Below To Invite Me!')
       .setColor('#22B0FA')
-      .addField("Invite", `[Click Here](https://discordapp.com/api/oauth2/authorize?client_id=489076647727857685&permissions=2146958839&scope=bot)`)
+      .addField("Invite", `[Click Here](https://discordapp.com/oauth2/authorize?client_id=489076647727857685&scope=bot&permissions=2146958847)`)
       .setTimestamp()
   msg.channel.send(inviteembed)
 
